b7e6f8b4dff08ddf9cf5c7234a5445df4ce46503
puneet.kataria <puneet.kataria@tothenew.com>
2026-06-13 17:47:05 +0530

    some incomplete questions

---
+++ src/main/java/org/puneet/project/code/patternlearning/twopointers/SquareOfSortedArray.java
@@ -0,0 +1,19 @@
+package org.puneet.project.code.patternlearning.twopointers;
+
+import java.util.Arrays;
+
+public class SquareOfSortedArray {
+    public static void main(String[] args) {
+        int[] sortedSquares = sortedSquaresBruteForce(new int[]{-4, -1, 0, 3, 10});
+        System.out.println(" sortedSquares " + Arrays.toString(sortedSquares));
+    }
+
+    public static int[] sortedSquaresBruteForce(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(result);
+        return result;
+    }
+}
